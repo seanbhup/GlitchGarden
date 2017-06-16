3a6e9d3a869fa7258b70edd5206034878ec3d627
Sean Bhupathi <seanbhupathi@gmail.com>
2017-06-16 15:45:15 -0400

    init commit, added splash screen, start menu, win/lose menu, 3 levels with grass canvas prefab. added music on splash and musicmanager for each level. added fade in script for the start menu. all buttons go to their respective counterparts right now.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+
+	public float autoLoadNextLevelAfter;
+	
+	void Start(){
+		if(autoLoadNextLevelAfter == 0){
+			
+		}
+		else{
+			Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+
+	public void LoadLevel(string name){
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest(){
+		Application.Quit();
+	}
+	
+	public void LoadNextLevel(){
+//	int index from build settings
+		
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+	
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+
+	private AudioSource audioSource;
+
+	void Awake(){
+		DontDestroyOnLoad(gameObject);
+//		Debug.Log("Don't destroy on load: " + name);
+	}
+	
+	void Start(){
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	void OnLevelWasLoaded(int level){
+		AudioClip thisLevelMusic = levelMusicChangeArray[level];
+//		Debug.Log("Playing clip: " + thisLevelMusic);
+
+		if(thisLevelMusic){
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+}
