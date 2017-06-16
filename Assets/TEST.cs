8a341d0e1355f5d41319dbaf41db2ba1336f1b84
Sean Bhupathi <seanbhupathi@gmail.com>
2017-06-16 15:59:01 -0400

    adding playerprefs starting with amastervolume

---
+++ Assets/TEST.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TEST : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print (PlayerPrefsManager.GetMasterVolume());
+		print (PlayerPrefsManager.SetMasterVolume(0.3f));
+		print (PlayerPrefsManager.GetMasterVolume());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
