52f0d273145418108608b06782e92398db925d10
Sean Bhupathi <seanbhupathi@gmail.com>
2017-06-30 14:58:04 -0400

    fixed some minor problems, prefabed the attackers and defenders. tested on level 2. goooooood shit

---
+++ Assets/SetStartVolume.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetStartVolume : MonoBehaviour {
+
+
+	private MusicManager musicManager;
+
+	// Use this for initialization
+	void Start () {
+		musicManager = GameObject.FindObjectOfType<MusicManager>();
+		if(musicManager){
+//			Debug.Log ("Found music manager" + musicManager);
+			float volume = PlayerPrefsManager.GetMasterVolume();
+			musicManager.SetVolume(volume);
+		}
+		else{
+			Debug.LogWarning("No music manager found in Start scene");
+		}
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
