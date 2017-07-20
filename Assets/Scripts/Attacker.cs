491ba8a1734eebc24114a5e0f4e7f9c3430d7596
Sean Bhupathi <seanbhupathi@gmail.com>
2017-07-20 15:06:00 -0400

    animations now move transition translate. added triggers and colliders on attackers and defenders. it logs when they trigger. updated animations with differing speed so jumping and walking look fluid. some other minor things

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour {
+
+	[Range(-1f,1.5f)]public float currentSpeed;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>();
+		myRigidbody.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
+	}
+	
+	void OnTriggerEnter2D (){
+		Debug.Log(name + " trigger enter");
+	}
+	
+	public void SetSpeed(float speed){
+		currentSpeed = speed;
+	}
+	
+	public void StrikeCurrentTarget (float damage){
+		Debug.Log(name + " caused damage: " + damage);
+ 	}
+	
+}
